27237bf5ce4c2342d40c93be5f618c42cea7b168
bing <bing@example.com>
2019-05-27 15:41:11 +0800

    修复bug: 编辑用户名时，如果用户名已注册，会发生错误

--- app/forms.py
+++ app/forms.py
@@ -33,3 +33,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired(message='请输入用户名!')])
     about_me = TextAreaField('关于我', validators=[Length(min=0, max=140)])
     submit = SubmitField('提交')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('用户名已存在，请使用不同的用户名')
