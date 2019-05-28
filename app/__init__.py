cd952e58bf4f5aa46d43b60b8f2c3c6875001a2c
bing <bing@example.com>
2019-05-28 10:53:34 +0800

    日期和时间

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-from config import Config
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from config import Config
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -19,6 +20,7 @@ login.login_view = 'login'
 
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import views, models, error
 
